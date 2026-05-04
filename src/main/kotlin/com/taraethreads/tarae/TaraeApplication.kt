d255f8712f8c24903a1b53bd18632da92486c42d
teho <xogh8755@gmail.com>
2026-05-04 18:14:21 +0900

    fix: JVM 타임존 KST 고정으로 시간 응답 오류 수정

--- src/main/kotlin/com/taraethreads/tarae/TaraeApplication.kt
+++ src/main/kotlin/com/taraethreads/tarae/TaraeApplication.kt
@@ -2,10 +2,12 @@ package com.taraethreads.tarae
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.util.TimeZone
 
 @SpringBootApplication
 class TaraeApplication
 
 fun main(args: Array<String>) {
+    TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"))
     runApplication<TaraeApplication>(*args)
 }
